8452217d887480e8dc4e21cdadfa27a3499448bd
Toshiaki Inahata <darwin49@gmail.com>
2022-07-23 23:02:29 +0900

    Update observable.go

--- rx/observable.go
+++ rx/observable.go
@@ -1,6 +1,9 @@
 package rx
 
+import "time"
+
 type Observable[T any] interface {
+	Delay(delay time.Duration) Observable[T]
 	Filter(predicate func(T) bool) Observable[T]
 	Subscribe(onNext func(T), onError func(err error), onCompleted func()) Subscription
 }
